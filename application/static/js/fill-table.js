266cf26d2111690dcb5eb13bdd66fbe61d87fa59
traptrip <aletni@gmail.com>
2022-05-29 23:54:50 +0500

    del refreshing + add testing notebook + change delete task button

--- application/static/js/fill-table.js
+++ application/static/js/fill-table.js
@@ -18,7 +18,7 @@ fetch("/tasks").then(function (response) {
             `            <td class="centred-td-style">\n` + result_meta_url +
             `            </td>\n` +
             `            <td class="centred-td-style">\n` +
-            `                <svg onclick="deleteTask(this)" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 512 512" width="16px" height="16px"><path fill="#E04F5F" d="M504.1,256C504.1,119,393,7.9,256,7.9C119,7.9,7.9,119,7.9,256C7.9,393,119,504.1,256,504.1C393,504.1,504.1,393,504.1,256z"></path><path fill="#FFF" d="M285,256l72.5-84.2c7.9-9.2,6.9-23-2.3-31c-9.2-7.9-23-6.9-30.9,2.3L256,222.4l-68.2-79.2c-7.9-9.2-21.8-10.2-31-2.3c-9.2,7.9-10.2,21.8-2.3,31L227,256l-72.5,84.2c-7.9,9.2-6.9,23,2.3,31c4.1,3.6,9.2,5.3,14.3,5.3c6.2,0,12.3-2.6,16.6-7.6l68.2-79.2l68.2,79.2c4.3,5,10.5,7.6,16.6,7.6c5.1,0,10.2-1.7,14.3-5.3c9.2-7.9,10.2-21.8,2.3-31L285,256z"></path></svg>\n` +
+            `                <svg onclick="deleteTask(this)" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 512 512" width="16px" height="16px"><rect style="fill:#E21B1B;" width="512" height="512"/><g><rect x="227.972" y="97.607" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -106.0462 255.9763)" style="fill:#FFFFFF;" width="55.991" height="316.781"/><rect x="97.652" y="228.001" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -106.0244 256.0289)" style="fill:#FFFFFF;" width="316.781" height="55.991"/></g></svg>\n` +
             `            </td>\n` +
             `        </tr>`);
     }
